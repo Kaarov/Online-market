1545e0fbdd46e2ddffd30aab6e4986087ed3532c
Anas Kaarov <anasbinkurban@gmail.com>
2023-03-24 02:30:34 +0600

    feat: added new project university

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO student (name, group_name, gpa) VALUES ('Anas', 'Com21B', 4);
-INSERT INTO student (name, group_name, gpa) VALUES ('Uluk', 'Com21B', 3);
-INSERT INTO student (name, group_name, gpa) VALUES ('Sam', 'Com21A', 3.4);
